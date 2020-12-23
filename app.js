d2e869cbe43b5ba23d1a77ecd660a891b4507ff6
Andriy Gonda <andriy.gonda@gmail.com>
2020-12-23 23:59:32 +0200

    added sequelize support

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 'use strict'
 const express = require('express');
-const {PORT} = require('./config')
+const helmet = require('helmet');
+const logger = require('morgan');
+
+const {PORT} = require('./settings');
+const {db} = require('./settings');
 
 const port = PORT || 5000
 
 const app = express()
 app.use(express.json())
+app.use(helmet());
+app.use(logger('dev'));
 
 app.listen(port, () => {
-    console.log(`Application running on port ${port}`)
+    console.log(db);
+    console.log(`Application running on port ${port}`);
 })
